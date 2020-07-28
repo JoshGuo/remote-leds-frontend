88fcdf010df024c43b20a073194593d510cf0626
Josh Guo <joshua.guo@stonybrook.edu>
2020-07-27 23:27:48 -0400

    Updates

---
+++ precache-manifest.d8ec648fa96a0bd7466f4c5bc6432b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57c150721a81ca77f467023d911e15f",
+    "url": "/remote-leds/index.html"
+  },
+  {
+    "revision": "1feade6cb5be19ce78a1",
+    "url": "/remote-leds/static/css/main.dafaebda.chunk.css"
+  },
+  {
+    "revision": "7e4eded5f916dcbe9e1d",
+    "url": "/remote-leds/static/js/2.5b48822b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/remote-leds/static/js/2.5b48822b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1feade6cb5be19ce78a1",
+    "url": "/remote-leds/static/js/main.19148caa.chunk.js"
+  },
+  {
+    "revision": "a5890c15fcd4bd82e2b5",
+    "url": "/remote-leds/static/js/runtime-main.95d3ae03.js"
+  }
+]);
